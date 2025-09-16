bc37ed819f3014609f63ad76dad6cde9b4e9eced
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-09-17 06:58:46 +0800

    Practcing more about dunder methods and best practices for classes with this methods

--- modelos/restaurant.py
+++ modelos/restaurant.py
@@ -1,30 +1,24 @@
 class Restaurant:
-    name = ''
-    category = ''
-    active = False
+      def __init__(self, name, category):
+            self.name = name
+            self.category = category
+            self.active = False
+      
+      def __str__(self):
+            return (f'''
+            Restaurant name: {self.name};
+            Restaurant category: {self.category};
+            Restaurant status: {'active' if self.active == True else 'inactive'}.\n
+            ''')
+      
+
     
-restaurant_1 = Restaurant()
-restaurant_1.name = 'BaysWater'
-restaurant_1.category = 'Italian'
-restaurant_1.active = False
+restaurant_1 = Restaurant('Bistro', 'Italian')
+restaurant_2 = Restaurant('Pizzeto', 'Pizza')
 
+print(restaurant_1)
+print(restaurant_2)
 
-restaurant_1.name = 'Bistro'
+restaurant_1.active = True
 
-print(f'''
-      Restaurant name: {restaurant_1.name};
-      Restaurant category: {restaurant_1.category};
-      Restaurant status: {'active' if restaurant_1.active == True else 'inactive'}.\n
-      ''')
-
-
-restaurant_2 = Restaurant()
-restaurant_2.name = 'Pizzeto'
-restaurant_2.category = 'Pizza'
-restaurant_2.active = True
-
-print(f'''
-      Restaurant name: {restaurant_2.name};
-      Restaurant category: {restaurant_2.category};
-      Restaurant status: {'active' if restaurant_2.active == True else 'inactive'}.\n
-      ''')
+print(restaurant_1)
