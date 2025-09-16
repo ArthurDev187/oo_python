c3928613ee21500fec734b71a45ddd5b409ece15
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-09-17 05:53:05 +0800

    First commit

---
+++ modelos/music.py
@@ -0,0 +1,12 @@
+class Music:
+    name = ''
+    artist = ''
+    duration = 0
+    
+
+music1 = Music()
+music1.name = 'Bohemian Rhapsody'
+music1.artist = 'Queen'
+music1.duration = 355
+
+print(f'Music: {music1.name}, Artist: {music1.artist}, duration: {music1.duration}')
